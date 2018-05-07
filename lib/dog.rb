f522353924a9d0ad0c5c94eb2de5f9c52773324e
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-05-07 00:05:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,1 @@
 # Your code goes here!
-class Dog
-  def instantiate(name)
-    @name = name
-  end
-    def name=(name)
-      @name = name
-    end
-
-    def name
-      @name
-    end
-    def bark
-      puts "woof!"
-    end
-  end
-
-fido = Dog.bark
